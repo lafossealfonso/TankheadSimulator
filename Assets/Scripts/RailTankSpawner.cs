4aeb9e5276120f3e06f6f39eeab2db178219e3e4
DESKTOP-11G0H42\User <alf.lafosse@gmail.com>
2024-07-04 22:56:43 +0930

    Enemy Spawner works flawlessly

--- Assets/Scripts/RailTankSpawner.cs
+++ Assets/Scripts/RailTankSpawner.cs
@@ -1,3 +1,5 @@
+using Cinemachine;
+using System.Collections;
 using UnityEngine;
 
 public class RailTankSpawner : MonoBehaviour
@@ -9,23 +11,51 @@ public class RailTankSpawner : MonoBehaviour
     [SerializeField] private float height;
     [SerializeField] private float speed; // Speed at which the object moves
     [SerializeField] private float arrivalThreshold; // Distance threshold for stopping
+    [SerializeField] private GameObject textGameObject;
+
+    [Header("Camera Settings")]
+
+    [SerializeField] private Transform mainCameraTransform;
+    [SerializeField] private CinemachineVirtualCamera virtualCamera;
+    [SerializeField] private Transform railTank;
+    [SerializeField] private Transform spiderTank;
+    [SerializeField] private Transform railTankCameraPosition;
+    [SerializeField] private TankMovement tankMovementScript;
+    private Transform lastCameraPosition;
 
     private void Start()
     {
         originalPosition = transform.position;
         objectToActivate.SetActive(false); // Ensure the object to activate starts deactivated
         railTankScript.enabled = false;
+        textGameObject.SetActive(false);
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player") && !isActivated)
         {
-            // Activate the object above its original position
-            objectToActivate.transform.position = originalPosition + Vector3.up * height;
-            objectToActivate.SetActive(true);
-            isActivated = true;
+            textGameObject.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                // Activate the object above its original position
+                objectToActivate.transform.position = originalPosition + Vector3.up * height;
+                objectToActivate.SetActive(true);
+                isActivated = true;
+                virtualCamera.m_Follow = railTankCameraPosition;
+                virtualCamera.m_LookAt = railTank;
+                tankMovementScript.enabled = false;
+                lastCameraPosition = mainCameraTransform;
+                mainCameraTransform.position = railTankCameraPosition.position;
+            }
         }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        textGameObject.SetActive(false);
     }
 
     private void Update()
@@ -41,8 +71,21 @@ public class RailTankSpawner : MonoBehaviour
                 // Ensure exact positioning at the end
                 objectToActivate.transform.position = originalPosition;
                 isActivated = false;
-                railTankScript.enabled = true;
+
+                StartCoroutine(WaitFor(2f));
+                
             }
         }
+    }
+
+    private IEnumerator WaitFor(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        railTankScript.enabled = true;
+        tankMovementScript.enabled = true;
+        virtualCamera.m_Follow = spiderTank;
+        virtualCamera.m_LookAt = spiderTank;
+        mainCameraTransform = lastCameraPosition;
+        Destroy(gameObject);
     }
 }
