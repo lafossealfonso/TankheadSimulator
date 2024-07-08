66db9d192af27a3334237105e75fb23f115d74b0
DESKTOP-11G0H42\User <alf.lafosse@gmail.com>
2024-07-08 13:23:22 +0930

    Scene Set

--- Assets/Scripts/HealPlayer.cs
+++ Assets/Scripts/HealPlayer.cs
@@ -6,14 +6,28 @@ public class HealPlayer : MonoBehaviour
 {
     private HealthSystem playerHealthSystem;
     [SerializeField] private int healAmount;
+    [SerializeField] GameObject text;
 
+    private void Start()
+    {
+        text.SetActive(false);
+    }
     private void OnTriggerStay(Collider other)
     {
         if(other.gameObject.tag == "Player" && Input.GetKey(KeyCode.E))
         {
+            text.SetActive(true);
             playerHealthSystem = other.GetComponent<HealthSystem>();
             playerHealthSystem.Heal((int)playerHealthSystem.health);
             Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            text.SetActive(false);
         }
     }
 }
